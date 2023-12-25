932a0bf82d75dff659755a1196b61da30f0a1ec7
Ismael Vilacian <ismaelv161@gmail.com>
2023-12-25 00:35:23 -0300

    Adjust the game screen, and establish a connection between the user and the server with socketio

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import http from 'http';
+import socketio from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
+const sockets = socketio(server)
 
 app.use(express.static('public'));
 
+sockets.on('connection', (socket) => {
+    const playerId = socket.id
+    console.log(`> Player connected: ${playerId}`)
+})
 
 server.listen(3000, () => {
     console.log('> server running');
