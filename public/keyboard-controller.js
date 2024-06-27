14b256ff0fdd13e059d5854e05f1f055ce774611
Ismael Vilacian <ismaelv161@gmail.com>
2024-06-26 21:04:22 -0300

    Adjust player movement

--- public/keyboard-controller.js
+++ public/keyboard-controller.js
@@ -18,18 +18,53 @@ export default function createKeyboardController(document) {
         }
     }
 
-    document.addEventListener('keydown', handleKeydown);
+    let animationFrameId;
+    let lastUpdateTime = 0;
+    const updateInterval = 5;
 
-    function handleKeydown(event) {
-        const keyPressed = event.key
+    const keysPressed = new Set();
 
-        const command = {
-            type: 'move-player',
-            playerId: state.playerId,
-            keyPressed
+    document.addEventListener('keydown', (event) => {
+        keysPressed.add(event.key);
+        
+        if (keysPressed.size === 1) {
+            requestAnimationFrame(startMovementLoop);
+        }
+    });
+
+    document.addEventListener('keyup', (event) => {
+        keysPressed.delete(event.key);
+        
+        if (keysPressed.size === 0) {
+            cancelAnimationFrame(animationFrameId);
+            lastUpdateTime = 0;
+        }
+    });
+
+    function startMovementLoop(timestamp) {
+        if (!lastUpdateTime) lastUpdateTime = timestamp;
+        const deltaTime = timestamp - lastUpdateTime;
+
+        if (deltaTime >= updateInterval) { 
+            processMovement();
+            lastUpdateTime = timestamp; 
         }
 
-        notifyAll(command)
+        if (keysPressed.size > 0) {
+            animationFrameId = requestAnimationFrame(startMovementLoop);
+        }
+    }
+
+    function processMovement() {
+        keysPressed.forEach((keyPressed) => {
+            const command = {
+                type: 'move-player',
+                playerId: state.playerId,
+                keyPressed
+            };
+
+            notifyAll(command);
+        });
     }
 
     return {
